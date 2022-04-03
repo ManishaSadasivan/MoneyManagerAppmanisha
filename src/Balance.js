78bef1016437092b1cab665e0ab797ff10b79c52
ManishaSadasivan <54832964+ManishaSadasivan@users.noreply.github.com>
2022-04-03 12:22:09 +0530

    latestupdate

--- src/Balance.js
+++ src/Balance.js
@@ -4,7 +4,7 @@ export default function Balance() {
   return (
     <div className="Balance">
     <div class="alert alert-success">
-        <header>Remaining : ₹7000 this month</header>
+        <header>Remaining : ₹ 7000 this month</header>
     </div>
 </div>
   )
